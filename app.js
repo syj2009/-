3ad99c36fb225c384192fc990d9ff99a5619f8e4
崔建伟 <cuijianwei2@astroway.net>
2018-03-20 09:10:08 +0800

    添加首页转发分享以及actionSheet弹框

--- app.js
+++ app.js
@@ -11,29 +11,29 @@ require('./lib/wxpage').A({
     wx.showShareMenu({
       withShareTicket: true
     })
-    wx.login({
-      success: function (res) {
-        var wxUserInfo = wx.getStorageSync('wxUserInfo')
-        wx.request({
-          url: that.data.host + '/api/Account/GetUserInfo',
-          data: {
-            'WeChatAccount': res.code,
-            'HeadImg': "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83er2O1yrwic2ntc5qy3qLpJic1ASYnzRFAicMbw1l10ic3d9hBDlXet34klvg4niaoFO1fYG8Ck0IvtHZvQ/0",
-            'UserName': "关关雎鸠",
-          },
-          method: 'POST',
-          dataType: 'json',
-          success: function (res) {
-            if (res.data.Msg) {
-              console.log("wxOpenId:" + res.data.Info.OpenId)
-              wx.setStorageSync('wxUserId', res.data.Info.UserId);
-              wx.setStorageSync('wxOpenId', res.data.Info.OpenId);
-              wx.setStorageSync('StudyNumber', res.data.Info.StudyNumber);
-            }
-          }
-        })
-      }
-    })
+    // wx.login({
+    //   success: function (res) {
+    //     var wxUserInfo = wx.getStorageSync('wxUserInfo')
+    //     wx.request({
+    //       url: that.data.host + '/api/Account/GetUserInfo',
+    //       data: {
+    //         'WeChatAccount': res.code,
+    //         'HeadImg': "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83er2O1yrwic2ntc5qy3qLpJic1ASYnzRFAicMbw1l10ic3d9hBDlXet34klvg4niaoFO1fYG8Ck0IvtHZvQ/0",
+    //         'UserName': "关关雎鸠",
+    //       },
+    //       method: 'POST',
+    //       dataType: 'json',
+    //       success: function (res) {
+    //         if (res.data.Msg) {
+    //           console.log("wxOpenId:" + res.data.Info.OpenId)
+    //           wx.setStorageSync('wxUserId', res.data.Info.UserId);
+    //           wx.setStorageSync('wxOpenId', res.data.Info.OpenId);
+    //           wx.setStorageSync('StudyNumber', res.data.Info.StudyNumber);
+    //         }
+    //       }
+    //     })
+    //   }
+    // })
   },
   onShow: function () {
     // Do something
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,26 +2,83 @@
 var P = require('../../lib/wxpage')
 P('index', {
   data: {
-    tabs: ["选项一", "选项二", "选项三"],
-    activeIndex: 1,
-    sliderOffset: 0,
-    sliderLeft: 0
+    //1：轮播图相关配置
+    imgUrls: [
+      '../../image/index/banner.png',
+      '../../image/index/banner.png',
+      '../../image/index/banner.png'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
   },
-  onLoad: function () {
-    var that = this;
-    wx.getSystemInfo({
+
+  onLaunch: function () {
+  },
+
+  /**
+   * 初次加载页面配置
+   * **/
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '快报名'
+    })
+    wx.showShareMenu({
+      withShareTicket: true
+    })
+    this.setData({
+      city: options.city
+    })
+    console.log("options.city$$$$$$$$$" + options.city)
+  },
+
+  /**
+     * 下拉刷新
+     * **/
+  onPullDownRefresh: function () {
+    //this.getPayCourseList();
+    wx.stopPullDownRefresh()
+  },
+
+  /**
+   * 跳转到模版列表页面
+   */
+  goModelList: function () {
+    wx.navigateTo({
+      url: "/pages/model-list/index",
+    })
+  },
+  /**
+   * 更多按钮点击事件
+   */
+  openAcctionSheet:function(){
+    wx.showActionSheet({
+      itemList: ['A', 'B', 'C'],
       success: function (res) {
-        that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
-        });
+        if (!res.cancel) {
+          console.log(res.tapIndex)
+        }
       }
     });
   },
-  tabClick: function (e) {
-    this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id
-    });
+  /**
+   * 按钮分享
+   */
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '自定义转发标题',
+      path: '/page/index/index',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
   }
 })
