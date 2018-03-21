fcf00f29f0ee92bcc18f89612b956e28d69b976c
mac_pro <gaoyang@astroway.net>
2018-03-21 15:21:31 +0800

    1.举报原因页面绘制以及部分逻辑添加 2.浏览失效页面 切图更换

--- pages/browse-failure/index.js
+++ pages/browse-failure/index.js
@@ -2,7 +2,34 @@
 var P = require('../../lib/wxpage')
 P('index', {
   data: {
-
+    showModal: true,
+    dataArray: [
+      {
+        isSelect: false,
+        id: 121,
+        name: "恶意营销、虚假宣传"
+      },
+      {
+        isSelect: false,
+        id: 2,
+        name: "谣言、社会负面、诈骗"
+      },
+      {
+        isSelect: false,
+        id: 3,
+        name: "色情、赌博、毒品"
+      },
+      {
+        isSelect: false,
+        id: 4,
+        name: "抄袭、侵权"
+      },
+      {
+        isSelect: false,
+        id: 511,
+        name: "违反国家法律政策、法律、法规"
+      }
+    ]
   },
 
   onLaunch: function () {
@@ -25,8 +52,41 @@ P('index', {
     wx.showToast({
       title: '制作我的招生方案',
     })
+  },
+
+  //选择举报原因
+  itemclick: function (e) {
+    var self = this;
+    var index = e.currentTarget.id;
+    var tempBool = self.data.dataArray[index].isSelect
+    self.data.dataArray[index].isSelect = !tempBool;
+    self.setData(
+      {
+        dataArray: self.data.dataArray
+      });
+  },
+
+  //提交举报原因
+  submitclick:function () {
+    var submitStr = "";
+    for (var i = 0; i < this.data.dataArray.length; i++){
+      if (this.data.dataArray[i].isSelect == true) {
+        if (i == this.data.dataArray.length - 1) {
+          submitStr += this.data.dataArray[i].id
+        } else {
+          submitStr += this.data.dataArray[i].id + ","
+        }
+      }
+    }
+    console.log("举报原因id ....", submitStr)
+    if (submitStr == ""){
+      wx.showToast({
+        title: '请选择举报原因',
+      })
+    }else {
+      this.setData({
+        showModal:false
+      });
+    }
   }
-
-  
-
 })
